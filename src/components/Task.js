7fa2a6ab6451a3f5416512ca78a86c159560b81a
James Heggs <james.heggs@techreturners.com>
2020-01-22 10:49:21 +0000

    Update UI based upon completed status

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,29 +1,45 @@
 import React from "react";
 
 class Task extends React.Component {
-
   deleteClicked = () => {
     this.props.deleteTaskFunc(this.props.item.id);
-  }
+  };
 
   doneClicked = () => {
     this.props.completedTaskFunc(this.props.item.id);
-  }
-
+  };
 
   render() {
-    return (
-      <div className="row taskRow">
-        <div className="col-12 col-md-8">
+    let description;
+
+    if (this.props.item.completed) {
+      description = (
+        <div className="col-12 col-md-8 completedTask">
           {this.props.item.description}
         </div>
+      );
+    } else {
+      description = (
+        <div className="col-12 col-md-8">{this.props.item.description}</div>
+      );
+    }
+
+    return (
+      <div className="row taskRow">
+        {description}
         <div className="col-6 col-md-2">
-          <button type="button" className="btn btn-success" onClick={this.doneClicked}>
-            Done
-          </button>
+          {!this.props.item.completed && (
+            <button type="button" className="btn btn-success" onClick={this.doneClicked}>
+              Done
+            </button>
+          )}
         </div>
         <div className="col-6 col-md-2">
-          <button type="button" className="btn btn-danger" onClick={this.deleteClicked}>
+          <button
+            type="button"
+            className="btn btn-danger"
+            onClick={this.deleteClicked}
+          >
             Delete
           </button>
         </div>
